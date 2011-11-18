1c67101d8c1286fe7690aba2b5708f9bf9438686
toastcfh <toastcfh@gmail.com>
2011-11-17 22:29:16 -0500

    shooter.mk: remove init.rc for now and add perms for usb devices per google
    left the init.rc blob in for now just not copying it over. should prolly just move needed stuff to the device.rc later.

--- shooter.mk
+++ shooter.mk
@@ -28,8 +28,6 @@ PRODUCT_COPY_FILES += \
     device/htc/shooter/recovery/sbin/htcbatt:recovery/root/sbin/htcbatt
 
 PRODUCT_COPY_FILES += \
-    device/htc/shooter/prebuilt/init:root/init \
-    device/htc/shooter/init.rc:root/init.rc \
     device/htc/shooter/init.shooter.rc:root/init.shooter.rc \
     device/htc/shooter/ueventd.shooter.rc:root/ueventd.shooter.rc
 
@@ -59,6 +57,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
