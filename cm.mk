b6eb163390c4102cd6f3a1a3d89cc918065ac64d
jared waters <kushdeck@gmail.com>
2012-07-20 19:40:00 -0700

    fix bootanimation size

--- cm.mk
+++ cm.mk
@@ -12,6 +12,8 @@ PRODUCT_MODEL := PG86100
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="sprint/htc_shooter/shooter:4.0.3/IML74K/130920.2:user/release-keys" PRIVATE_BUILD_DESC="2.08.651.2 CL130920 release-keys"
 
+TARGET_BOOTANIMATION_NAME := vertical-540x960
+
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Shooter
 -include vendor/cm/config/common_versions.mk
